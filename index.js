e2c979d9c35459839d128318931ddc3510907cbb
emraftis <82290450+emraftis@users.noreply.github.com>
2021-08-27 17:41:39 -0600

    fixed routes for endpoints

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ require("dotenv").config();
 app.use(cors());
 
 // fetch 2019 account transactions API endpoint
-app.get("/txns/2019/:wallet", async (req, res) => {
+app.get("/2019/:wallet", async (req, res) => {
   const walletID = req.params.wallet;
   const action = req.query.action;
   const startBlock = 6990337;
   const endBlock = 9194676;
   try {
     const results = await loadTxns(walletID, startBlock, endBlock, action);
-    console.log(results);
     res.send(results);
   } catch (e) {
     console.log(e);
@@ -23,14 +22,13 @@ app.get("/txns/2019/:wallet", async (req, res) => {
 });
 
 // fetch 2020 account transactions API endpoint
-app.get("/txns/2020/:wallet", async (req, res) => {
+app.get("/2020/:wallet", async (req, res) => {
   const walletID = req.params.wallet;
   const action = req.query.action;
   const startBlock = 9194677;
   const endBlock = 11566959;
   try {
     const results = await loadTxns(walletID, startBlock, endBlock, action);
-    console.log(results);
     res.send(results);
   } catch (e) {
     console.log(e);
@@ -55,19 +53,19 @@ app.get("/2021/:wallet", async (req, res) => {
 
 //to paginate, add '&page=<page#>&offset=<max#results>' to queryString (as per docs)
 async function loadTxns(walletID, startBlock, endBlock, action) {
-  console.log("start loading txns");
   let txnList = null;
   const txns = await axios.get(
     `https://api.etherscan.io/api?module=account&action=${action}&address=${walletID}&startblock=${startBlock}&endblock=${endBlock}&sort=asc&apikey=${process.env.ETHERSCAN_KEY}`
   );
   txnList = txns.data.result;
+
   //filter txnList for results only where "from" === "walletID"
   txnList = txnList.filter(function (x) {
     return x.from === walletID.toLowerCase();
   });
 
   //loop through txnList for each transaction.timeStamp
-  //convert timeStamp date object
+  //convert timeStamp to date object
   //query coinbase for daily spot price
   //add spot price to each transaction in txnList.
   for (let i = 0; i < txnList.length; i++) {
