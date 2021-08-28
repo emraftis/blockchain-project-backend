2162358c3ddef85a48123c56d06d1d6f734837d2
Evan Raftis <evanraftis@gmail.com>
2021-08-27 23:12:20 -0600

    updates

--- index.js
+++ index.js
@@ -96,5 +96,5 @@ app.get(/.*/, (req, res) => {
 });
 
 //listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("Listening on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Listening on port 3000"));
